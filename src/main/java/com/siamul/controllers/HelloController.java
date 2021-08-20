090ed078bb30efa55aca901edf70fcaf67f3a2d3
traveller-42 <siamul.islam@northsouth.edu>
2021-08-20 11:06:10 +0600

    updated

--- src/main/java/com/siamul/controllers/HelloController.java
+++ src/main/java/com/siamul/controllers/HelloController.java
@@ -1,16 +1,26 @@
 package com.siamul.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("name","siam");
         return "index";
     }
     @RequestMapping("/about")
     public String about(){
         return "about";
+    }
+    @RequestMapping("/help")
+    public ModelAndView help(){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.addObject("name","siam");
+        modelAndView.addObject("age",18);
+        return modelAndView ;
     }
 }
--- src/main/webapp/WEB-INF/views/help.jsp
+++ src/main/webapp/WEB-INF/views/help.jsp
@@ -2,17 +2,16 @@
   Created by IntelliJ IDEA.
   User: siamul
   Date: ২০/৮/২১
-  Time: ১০:৩৩ AM
+  Time: ১১:০২ AM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>SIAMUL</title>
+    <title>HELP PAGE</title>
 </head>
 <body>
-  <h1>This is a Home Page</h1>
-  <h2>This should be Called From Controller</h2>
-   <h3>My Name Is ${name}</h3>
+   <h1>This is ${name}</h1>
+   <p>My Age is ${age}</p>
 </body>
 </html>
